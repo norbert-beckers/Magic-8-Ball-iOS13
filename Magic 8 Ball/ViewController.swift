61a804ae9e87bb62fdfa818768287815aa9a5ebc
Norbert Beckers <norbert@norbertbeckers.com>
2020-04-21 17:23:31 +0200

    Link up the design with code

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -11,8 +11,11 @@ import UIKit
 class ViewController: UIViewController {
     
     let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
-
-
+    @IBOutlet weak var MagicEightBall: UIImageView!
+    
+    @IBAction func AskButtonPressed(_ sender: UIButton) {
+    }
+    
 
 }
 
